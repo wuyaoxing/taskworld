233187462ad4528dc9c2c80b08a99a0ca0331cea
wuyaoxing <82565097@qq.com>
2018-03-29 18:08:58 +0800

    update ProjectListPage

--- src/react/components/projects/project-list/ProjectListPage.react.js
+++ src/react/components/projects/project-list/ProjectListPage.react.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import SubHeader from '../../../../core/app-layout/SubHeader.react'
+import SubLayout from '../../../../core/app-layout/SubLayout.react'
 
 class ProjectListPage extends React.PureComponent {
+    renderTopBar = () => {
+        return (
+            <SubHeader
+                left={<div>Left</div>}
+                center={<div>ProjectListPage</div>}
+                right={<div>Right</div>}
+            />
+        )
+    }
+
     render() {
         return (
             <section className="project-list-page">
-                <h1>ProjectListPage</h1>
+                <SubLayout header={this.renderTopBar()}>
+                    <h2>ProjectListBody</h2>
+                </SubLayout>
             </section>
         )
     }
