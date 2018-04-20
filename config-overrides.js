520bf67f13eddfaf0582ee2598c06ea5c82c8e90
wuyaoxing <82565097@qq.com>
2018-04-20 18:09:28 +0800

    chore(webpack): require webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -2,7 +2,9 @@ const rewireEslint = require('react-app-rewire-eslint')
 const rewireLess = require('react-app-rewire-less')
 
 module.exports = function override(config, env) {
+    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
     config = rewireEslint(config, env)
     config = rewireLess(config, env)
+    config.plugins.push(new BundleAnalyzerPlugin())
     return config
 }
