59679c8edcd357dd8732ce3b875339b656810339
wuyaoxing <82565097@qq.com>
2018-04-22 08:58:46 +0800

    chore(pages): asyncComponent

--- src/core/frontend-app/Pages.js
+++ src/core/frontend-app/Pages.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import LoginPage from 'core/app-login-page/LoginPage.react'
-import ErrorPage from 'react/components/errors/ErrorPage.react'
+
+import lazyRouteRenderer from 'react/lazyRouteRenderer'
+
+function asyncComponent(asyncComponent, props = {}) {
+    const Component = lazyRouteRenderer(asyncComponent)
+    return <Component {...props} />
+}
 
 export function showLoginPage() {
-    return ReactDOM.render(<LoginPage />, document.getElementById('root'))
+    return ReactDOM.render(
+        asyncComponent(() =>
+            import(/* webpackChunkName: "app-login-page" */ 'core/app-login-page/LoginPage.react')
+        ),
+        document.getElementById('root')
+    )
 }
 
 export function showNotFoundPage() {
-    return ReactDOM.render(<ErrorPage />, document.getElementById('root'))
+    return ReactDOM.render(
+        asyncComponent(() =>
+            import(/* webpackChunkName: "app-error-page" */ 'react/components/errors/ErrorPage.react')
+        ),
+        document.getElementById('root')
+    )
 }
